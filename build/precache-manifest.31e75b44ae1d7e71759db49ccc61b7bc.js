f89fb2e7286672e14f3b3ab4500c1aa8b276724a
Josh Harris <49429928+jharris711@users.noreply.github.com>
2021-02-14 01:07:40 -0500

    remove outer dir

---
+++ build/precache-manifest.31e75b44ae1d7e71759db49ccc61b7bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83eba8722c174b468a24f7c9576a4df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c429fec3c426bd363a64",
+    "url": "/static/css/2.22680857.chunk.css"
+  },
+  {
+    "revision": "4973e172d5631375d183",
+    "url": "/static/css/main.b15d37cf.chunk.css"
+  },
+  {
+    "revision": "c429fec3c426bd363a64",
+    "url": "/static/js/2.f37615b5.chunk.js"
+  },
+  {
+    "revision": "6c8011a71c839a06a9876ac230f15d8e",
+    "url": "/static/js/2.f37615b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4973e172d5631375d183",
+    "url": "/static/js/main.0b1a06bb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
